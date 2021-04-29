99ea5a7ae4aef6f202ff0d6b8fff906c00176f26
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-29 15:23:31 +0800

    Add files via upload

---
+++ leetcode_no633_平方数之和_w1.cpp
@@ -0,0 +1,98 @@
+﻿/* ------------------------------------------------------------------|
+给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+
+ 
+
+示例 1：
+
+输入：c = 5
+输出：true
+解释：1 * 1 + 2 * 2 = 5
+示例 2：
+
+输入：c = 3
+输出：false
+示例 3：
+
+输入：c = 4
+输出：true
+示例 4：
+
+输入：c = 2
+输出：true
+示例 5：
+
+输入：c = 1
+输出：true
+ 
+
+提示：
+
+0 <= c <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sum-of-square-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：5.8 MB, 在所有 C++ 提交中击败了72.03%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+bool judgeSquareSum(int c) {
+    for (int i = 0; i <= sqrt(c); ++i) {
+        double b = sqrt(c - i * i);
+        if ((int)b == b) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    judgeSquareSum(18);
+}
